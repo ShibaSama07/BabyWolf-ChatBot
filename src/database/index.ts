c9cd4298669e4a0bc5a9c51eca968a494b8657f4
ShibaMini <shibamini07@gmail.com>
2023-04-18 23:30:27 +0700

    hanle thread-name + changed form data

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,34 +1,42 @@
 import { Sequelize, DataTypes, Model, type InferAttributes, type InferCreationAttributes } from "sequelize";
 
 const sequelize = new Sequelize({
-    storage: './database/data.sqlite',
+    storage: 'data.sqlite',
     dialect: 'sqlite',
     logging: false
 })
 
-interface ChannelMap extends Model<InferAttributes<ChannelMap>, InferCreationAttributes<ChannelMap>> {
+export interface IFCAU_ChannelMap extends Model<InferAttributes<IFCAU_ChannelMap>, InferCreationAttributes<IFCAU_ChannelMap>> {
     channelID: string;
     threadID: string;
+    data: {
+        enableGlobalThreads: boolean;
+        allowThreads: string[];
+        denyThreads: string[];
+    }
 }
 
-interface Thread extends Model<InferAttributes<Thread>, InferCreationAttributes<Thread>> {
+export interface IFCAU_Thread extends Model<InferAttributes<IFCAU_Thread>, InferCreationAttributes<IFCAU_Thread>> {
     threadID: string;
     threadName: string;
 }
 
-export const ChannelMap = sequelize.define<ChannelMap>('ChannelMaps', {
+export const ChannelMap = sequelize.define<IFCAU_ChannelMap>('ChannelMaps', {
     channelID: {
         type: DataTypes.STRING,
         primaryKey: true
     },
     threadID: {
         type: DataTypes.STRING
+    },
+    data: {
+        type: DataTypes.JSON
     }
 }, {
     timestamps: false
 })
 
-export const Thread = sequelize.define<Thread>('Threads', {
+export const Thread = sequelize.define<IFCAU_Thread>('Threads', {
     threadID: {
         type: DataTypes.STRING,
         primaryKey: true
